4718617174ba9d5482e521e3250f0cc3cca12e11
lstranskyTGM <104894882+lstranskyTGM@users.noreply.github.com>
2023-09-25 14:09:58 +0200

    Xml files update

--- app/src/main/java/com/example/lstransky/sew_7a_calculator/MainActivity.java
+++ app/src/main/java/com/example/lstransky/sew_7a_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lstransky.sew_7a_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+    }
+
+    public void memoryStore(View view) {
+    }
+
+    public void memoryRead(View view) {
     }
 }
